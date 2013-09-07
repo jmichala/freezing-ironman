3eb2d8af88d29e8330d14fa3548a88c402bc12ab
hemanthkini <hemanth.kini@gmail.com>
2013-09-07 01:55:22 -0400

    Update Nuance.java

--- MorningOrganizer/src/com/pennapps/morningorganizer/Nuance.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/Nuance.java
@@ -3,38 +3,40 @@ package com.pennapps.morningorganizer;
 import android.content.Context;
 import android.os.Handler;
 
+import com.nuance.nmdp.speechkit.SpeechError;
 import com.nuance.nmdp.speechkit.SpeechKit;
 import com.nuance.nmdp.speechkit.Vocalizer;
 
-public class Nuance {
+public class Nuance implements Vocalizer.Listener {
 
 	public static final String TTS_KEY = "com.nuance.nmdp.sample.tts";	 //to change later?
     
-    private Vocalizer _vocalizer;
-    private Object _lastTtsContext = null;
+    private static Vocalizer _vocalizer;
+    private static Object _lastTtsContext = null;
     
 	
 	private static SpeechKit _speechKit;
     
     // Allow other activities to access the SpeechKit instance.
-    static SpeechKit getSpeechKit()
+    SpeechKit getSpeechKit()
     {
         return _speechKit;
     }
     
-    static void initializeSpeechKit(Context appContext)
+    void initializeSpeechKit(Context appContext, Handler handler)
     {
     	if (_speechKit == null)
     	{
-    		  //_speechKit = SpeechKit.initialize(appContext, NuanceAppInfo.SpeechKitAppId, NuanceAppInfo.SpeechKitServer, NuanceAppInfo.SpeechKitPort, NuanceAppInfo.SpeechKitSsl, NuanceAppInfo.SpeechKitApplicationKey);
+    		  _speechKit = SpeechKit.initialize(appContext, NuanceAppInfo.SpeechKitAppId, NuanceAppInfo.SpeechKitServer, NuanceAppInfo.SpeechKitPort, NuanceAppInfo.SpeechKitSsl, NuanceAppInfo.SpeechKitApplicationKey);
                _speechKit.connect();
+               initializeTheVocalizer(appContext, handler);
     	}
     	
     }
     
     void initializeTheVocalizer(Context appContext, Handler handler)
     {
-    	_vocalizer = (Vocalizer) _speechKit.createVocalizerWithLanguage("en_US", (Vocalizer.Listener) appContext, handler);
+    	_vocalizer = (Vocalizer) _speechKit.createVocalizerWithLanguage("en_US", this, handler);
     }
     
     void speakTheString(String stringToSay, Context appContext)
@@ -42,7 +44,7 @@ public class Nuance {
     	_vocalizer.speakString(stringToSay, appContext);
     }
     
-    static void closeSpeechKit()
+    void closeSpeechKit()
     {
     	if (_speechKit != null)
     	{
@@ -50,5 +52,18 @@ public class Nuance {
     		_speechKit.release();
     	}
     }
+
+	@Override
+	public void onSpeakingBegin(Vocalizer arg0, String arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSpeakingDone(Vocalizer arg0, String arg1, SpeechError arg2,
+			Object arg3) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
